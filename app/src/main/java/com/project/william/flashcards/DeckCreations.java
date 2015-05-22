f4bd9501d57eb6295fca11697d20ba87732624b0
wbong <wbong@sfu.ca>
2015-05-22 13:33:02 -0700

    Setup initial classes

--- app/src/main/java/com/project/william/flashcards/DeckCreations.java
+++ app/src/main/java/com/project/william/flashcards/DeckCreations.java
@@ -1,9 +1,16 @@
 package com.project.william.flashcards;
 
 import android.app.Activity;
+import android.os.Bundle;
 
 /**
  * Created by William on 2015-05-22.
  */
 public class DeckCreations extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.deck_create_screen);
+    }
 }
--- app/src/main/java/com/project/william/flashcards/HelpScreen.java
+++ app/src/main/java/com/project/william/flashcards/HelpScreen.java
@@ -6,11 +6,11 @@ import android.os.Bundle;
 /**
  * Created by William on 2015-05-22.
  */
-public class ViewDecks extends Activity {
+public class HelpScreen extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.deck_view_screen);
+        setContentView(R.layout.help_screen);
     }
 }
